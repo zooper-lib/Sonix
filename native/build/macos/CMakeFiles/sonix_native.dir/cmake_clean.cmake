28125d4e93de80e8de60beaf6a84779b5ee5ac31
Daniel <daniel@zooper.dev>
2025-09-19 22:09:40 +0200

    Created macOS lib

---
+++ native/build/macos/CMakeFiles/sonix_native.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sonix_native.dir/src/aac/aac_decoder.c.o"
+  "CMakeFiles/sonix_native.dir/src/aac/aac_decoder.c.o.d"
+  "CMakeFiles/sonix_native.dir/src/sonix_native.c.o"
+  "CMakeFiles/sonix_native.dir/src/sonix_native.c.o.d"
+  "CMakeFiles/sonix_native.dir/src/stb_vorbis_wrapper.c.o"
+  "CMakeFiles/sonix_native.dir/src/stb_vorbis_wrapper.c.o.d"
+  "libsonix_native.dylib"
+  "libsonix_native.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sonix_native.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
